fca9861d2d340a92d9dc32588001c50c37545d5a
mrpitch <mrpitch@outlook.com>
2024-06-18 20:36:11 +0200

    feat: cleanup

--- src/api/editorial-page/controllers/editorial-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * editorial-page controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::editorial-page.editorial-page');
--- src/api/health-department/routes/health-department.ts
+++
@@ -1,7 +0,0 @@
-/**
- * health-department router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::health-department.health-department');
