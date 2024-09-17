bf3a0a4387294efcdd0a72fbb491f8eaf83e909f
mrpitch <mrpitch@outlook.com>
2024-09-17 16:57:07 +0200

    feat: update error message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
+import {errors} from '@strapi/utils'
+
+const {ApplicationError} = errors
 
 export default {
   /**
@@ -16,5 +19,20 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ["plugin::upload.file"],
+
+      beforeCreate(event) {
+
+        const { data, where, select, populate } = event.params;
+
+        console.log("mime type: ", data?.mime);
+        console.log("ext: ", data?.ext);
+        if (data?.mime === "image/svg+xml") {
+          throw new ApplicationError(`Invalid mime type: ${data?.mime}`);
+        }
+      },
+    });
+  },
 };
